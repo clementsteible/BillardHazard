8346570b8a95d4894d8816ad4ce502face5b0518
clementsteible <Clement_S@PC-N9NXCV016993>
2023-06-26 11:05:48 +0200

    Init Repository<T>

---
+++ BillardHazard/Repositories/RuleRepository.cs
@@ -0,0 +1,58 @@
+﻿using BillardHazard.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillardHazard.Repositories
+{
+    public class Repository<T> where T : class
+    {
+        private readonly BhContext _dbContext;
+
+        public Repository(BhContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<T> GetAll()
+        {            
+            return _dbContext.Set<T>().ToList();
+        }
+
+        public T? FindById(int id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
+
+        public void Delete(int id)
+        {
+            T? entity = FindById(id);
+
+            if (entity != null)
+            {
+                _dbContext.Remove(entity);
+                _dbContext.SaveChanges();
+                Console.WriteLine($"{typeof(T).Name} n°{id} supprimée !");
+            }
+            else
+            {
+                Console.WriteLine($"{typeof(T).Name} n°{id} n'existe pas !");
+            }
+        }
+
+        public void Delete(T entity){ 
+            _dbContext.Remove(entity);
+            Console.WriteLine($"{typeof(T).Name} supprimée !");
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+            Console.WriteLine($"{typeof(T).Name} modifiée !");
+        }
+
+        public void Create(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            Console.WriteLine($"Nouvelle {typeof(T).Name} créée !");
+        }
+    }
+}
